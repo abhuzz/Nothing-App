1614d5a1ccbb7b4474f757a3a5b9721d31fac2d8
Tomasz Szulc <mail@szulctomasz.com>
2014-11-21 00:10:53 +0100

    Animation duration update

--- Nothing/UIView_Extension.swift
+++ Nothing/UIView_Extension.swift
@@ -15,7 +15,7 @@ extension UIView {
         animation.toValue = 0
         animation.fromValue = NSValue(CATransform3D: CATransform3DIdentity)
         animation.toValue = NSValue(CATransform3D: CATransform3DMakeScale(0.95, 0.95, 1.0))
-        animation.duration = 0.2
+        animation.duration = 0.15
         animation.autoreverses = true
         self.layer.addAnimation(animation, forKey: nil)
     }
