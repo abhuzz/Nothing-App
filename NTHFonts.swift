cc59dd79f1fac13d20ff518415addd72d688a09e
Tomasz Szulc <mail@szulctomasz.com>
2014-12-07 22:42:07 +0100

    Add fonts for inbox view

---
+++ NTHFonts.swift
@@ -0,0 +1,40 @@
+//
+//  NTHFonts.swift
+//  Nothing
+//
+//  Created by Tomasz Szulc on 07/12/14.
+//  Copyright (c) 2014 Tomasz Szulc. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    /**
+        Font names
+    */
+    
+    private class func NTHRegularFontName() -> String {
+        return "HelveticaNeue-Regular"
+    }
+    
+    private class func NTHLightFontName() -> String {
+        return "HelveticaNeue-Light"
+    }
+    
+    private class func NTHMediumFontName() -> String {
+        return "HelveticaNeue-Medium"
+    }
+    
+    /**
+        Fonts
+    */
+    
+    class func NTHInboxCellTitleFont() -> UIFont {
+        return UIFont(name: NTHRegularFontName(), size: 32)!
+    }
+    
+    class func NTHInboxCellDescriptionFont() -> UIFont {
+        return UIFont(name: NTHLightFontName(), size: 12)!
+    }
+}
